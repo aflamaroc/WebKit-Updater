f549da573091ca32233769764bae61dd23676da6
Rayan Mohammed Majdi <yaylelhoel@gmail.com>
2026-08-01 14:01:53 -0400

    Initial Commit

---
+++ WebKit Updater/WebKit_Updater.h
@@ -0,0 +1,15 @@
+//
+//  WebKit_Updater.h
+//  WebKit Updater
+//
+//  Created by Rayan Mohammed Majdi on 8/1/26.
+//  Copyright (c) 2026 Rayan Mohammed Majdi. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface WebKit_Updater : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ WebKit Updater/WebKit_Updater.m
@@ -0,0 +1,17 @@
+//
+//  WebKit_Updater.m
+//  WebKit Updater
+//
+//  Created by Rayan Mohammed Majdi on 8/1/26.
+//  Copyright (c) 2026 Rayan Mohammed Majdi. All rights reserved.
+//
+
+#import "WebKit_Updater.h"
+
+@implementation WebKit_Updater
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
