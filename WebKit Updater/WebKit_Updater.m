a8c626e99538512a1a9f31d6fb03bc6288d85b7a
Rayan Mohammed Majdi <yaylelhoel@gmail.com>
2026-08-02 16:15:54 -0400

    hide name 😬

--- WebKit Updater/WebKit_Updater.m
+++ WebKit Updater/WebKit_Updater.m
@@ -2,8 +2,7 @@
 //  WebKit_Updater.m
 //  WebKit Updater
 //
-//  Created by Rayan Mohammed Majdi on 8/1/26.
-//  Copyright (c) 2026 Rayan Mohammed Majdi. All rights reserved.
+//  do whatever.
 //
 
 #import "WebKit_Updater.h"
