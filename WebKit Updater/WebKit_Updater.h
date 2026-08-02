72af74bff6c231cf33eda68ad129be48aaae305f
Rayan Mohammed Majdi <yaylelhoel@gmail.com>
2026-08-02 16:09:50 -0400

    Final UI changes, if a problem occurs, revert to this commit

--- WebKit Updater/WebKit_Updater.h
+++ WebKit Updater/WebKit_Updater.h
@@ -10,7 +10,16 @@
 #import <SecurityInterface/SFAuthorizationView.h>
 
 @interface WebKit_Updater : NSPreferencePane
+
 @property (weak) IBOutlet SFAuthorizationView *authView;
+
+@property (weak) IBOutlet NSButton *s7OTAbutton;
+@property (weak) IBOutlet NSButton *s7onDiskButton;
+@property (weak) IBOutlet NSButton *dlWebKitButton;
+@property (strong) IBOutlet NSPopover *helpPopover;
+
 - (void)mainViewDidLoad;
+- (void)authorizationViewDidDeauthorize:(SFAuthorizationView *)view;
+- (void)authorizationViewDidAuthorize:(SFAuthorizationView *)view;
 
 @end
--- WebKit Updater/WebKit_Updater.m
+++ WebKit Updater/WebKit_Updater.m
@@ -12,12 +12,30 @@
 - (IBAction)bugReport:(id)sender {
 	
 }
+- (IBAction)showHelpPopover:(id)sender {
+	[_helpPopover showRelativeToRect:[sender bounds] ofView:sender preferredEdge:NSMaxYEdge];
+}
 
 - (void)mainViewDidLoad
 {
 	[_authView setString:"system.preferences"];
 	[_authView setAutoupdate:YES];
+	[_authView setDelegate:self];
 	[_authView updateStatus:self];
+	_helpPopover.contentViewController = [[NSViewController alloc] initWithNibName:@"popover" bundle:[NSBundle bundleForClass:[self class]]];
+	;
+}
+
+- (void) authorizationViewDidAuthorize:(SFAuthorizationView *)view{
+	NSLog(@"i can doo what you want me to dooo");
+	[[_authView authorization] authorizationRef];
+	[_s7onDiskButton setEnabled:YES];
+	
+}
+
+-(void) authorizationViewDidDeauthorize:(SFAuthorizationView *)view{
+	NSLog(@"i cannot do what you want me to doooo");
+	[_s7onDiskButton setEnabled:NO];
 }
 
 @end
